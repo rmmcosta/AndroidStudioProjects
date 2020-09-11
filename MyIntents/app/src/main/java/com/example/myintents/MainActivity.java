2320e5ec16f1d6402641916f27a76345842eb119
rmmcosta <ricardocosta101085@gmail.com>
2020-09-11 08:34:25 +0100

    more on implicit intents and how to use chooser

--- MyIntents/app/src/main/java/com/example/myintents/MainActivity.java
+++ MyIntents/app/src/main/java/com/example/myintents/MainActivity.java
@@ -7,10 +7,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import static android.content.Intent.createChooser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,5 +49,25 @@ public class MainActivity extends AppCompatActivity {
             Toast toast = Toast.makeText(getApplicationContext(), myName, Toast.LENGTH_SHORT);
             toast.show();
         }
+    }
+
+    public void sendMessageOutside(View view) {
+        startActivity(getPlainTextIntentForMessage());
+    }
+
+    private Intent getPlainTextIntentForMessage() {
+        EditText editText = findViewById(R.id.etMessage);
+        String message = editText.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TITLE, "Message from MyIntents");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Inputted by the User");
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        return intent;
+    }
+
+    public void sendWithChooser(View view) {
+        Intent chooserIntent = createChooser(getPlainTextIntentForMessage(), "Send message with...");
+        startActivity(chooserIntent);
     }
 }
