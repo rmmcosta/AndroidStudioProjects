2ee5592528cef3e3e2da32926830920f47f82305
rmmcosta <ricardocosta101085@gmail.com>
2020-09-29 08:56:27 +0100

    quit developing backpro, since in most recent devices only system apps can access the usage stats. And it's impossible for me to develop a system app without rooting the device.

--- BackPro/app/src/main/java/com/example/backpro/MainActivity.java
+++ BackPro/app/src/main/java/com/example/backpro/MainActivity.java
@@ -1,23 +1,48 @@
 package com.example.backpro;
 
 import android.app.ActivityManager;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.UserManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
+
+class Killapp {
+    private int pid;
+    private String name;
+
+    public Killapp(int pid, String name) {
+        this.pid = pid;
+        this.name = name;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<ActivityManager.RunningAppProcessInfo> runningApps;
+    private List<Killapp> runningApps;
     private ActivityManager activityManager;
     private RecyclerView rvListApps;
     private RecyclerView.Adapter adapterListApps;
@@ -32,8 +57,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getRunningApps() {
-        activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-        runningApps = activityManager.getRunningAppProcesses();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            assignRunningApps((UsageStatsManager) getSystemService(USAGE_STATS_SERVICE));
+        } else {
+            assignRunningApps((ActivityManager) getSystemService(ACTIVITY_SERVICE));
+        }
         rvListApps = findViewById(R.id.rvListApps);
         adapterListApps = new AdapterListApps();
         rvListApps.setAdapter(adapterListApps);
@@ -42,6 +70,31 @@ public class MainActivity extends AppCompatActivity {
         int appsCount = rvListApps.getAdapter().getItemCount();
         tvCount = findViewById(R.id.tvCount);
         tvCount.setText(String.valueOf(appsCount));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void assignRunningApps(UsageStatsManager mUsageStatsManager) {
+        UserManager userManager = (UserManager) this.getSystemService(Context.USER_SERVICE);
+        boolean isUserUnlocked = userManager.isUserUnlocked();
+        Toast toast = Toast.makeText(this, "The user is unlocked:" + isUserUnlocked, Toast.LENGTH_SHORT);
+        toast.show();
+        runningApps = new ArrayList<>();
+        long currentTime = System.currentTimeMillis();
+        // get usage stats for the last 10 seconds
+        List<UsageStats> stats = mUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, currentTime - 1000 * 10, currentTime);
+        // search for app with most recent last used time
+        if (stats != null) {
+            for (UsageStats usageStats : stats) {
+                runningApps.add(new Killapp(-1, usageStats.getPackageName()));
+            }
+        }
+    }
+
+    private void assignRunningApps(ActivityManager activityManager) {
+        runningApps = new ArrayList<>();
+        for (ActivityManager.RunningAppProcessInfo app : activityManager.getRunningAppProcesses()) {
+            runningApps.add(new Killapp(app.pid, app.processName));
+        }
     }
 
     public void refreshApps(View view) {
@@ -70,9 +123,9 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull VHolderListApps holder, int position) {
-            ActivityManager.RunningAppProcessInfo app = runningApps.get(position);
+            Killapp app = runningApps.get(position);
             TextView textView = holder.tvApp;
-            textView.setText(app.processName);
+            textView.setText(app.getName());
         }
 
         @Override
