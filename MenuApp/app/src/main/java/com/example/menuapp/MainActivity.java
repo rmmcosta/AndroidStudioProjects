f3c53c332d5473795cb9b20571190a359244abe2
rmmcosta <ricardocosta101085@gmail.com>
2020-09-30 09:02:55 +0100

    started working with menus

---
+++ MenuApp/app/src/main/java/com/example/menuapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.menuapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast toast = Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT);
+        toast.show();
+        return true;
+    }
+}
