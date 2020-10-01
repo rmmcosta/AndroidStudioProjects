3c1f3e00ab8532f85204abe7f8242ff31c257302
rmmcosta <ricardocosta101085@gmail.com>
2020-10-01 08:54:55 +0100

    contextual menus

--- MenuApp/app/src/main/java/com/example/menuapp/MainActivity.java
+++ MenuApp/app/src/main/java/com/example/menuapp/MainActivity.java
@@ -5,17 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.zip.Inflater;
+
+import static com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_SHORT;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvHello;
+    private TextView tvCenas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvHello = findViewById(R.id.tvHello);
+        tvCenas = findViewById(R.id.tvCenas);
+        registerForContextMenu(tvHello);
+        registerForContextMenu(tvCenas);
     }
 
     @Override
@@ -26,9 +43,85 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context_operations, menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         Toast toast = Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT);
         toast.show();
         return true;
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        int viewId = item.getActionView().getId();
+        switch (itemId) {
+            case R.id.itemShow:
+                showDetail(viewId);
+                return true;
+            case R.id.itemEdit:
+                editDetail(viewId);
+                return true;
+            case R.id.itemDelete:
+                deleteDetail(viewId);
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    private void editDetail(int viewId) {
+        String message;
+        switch (viewId) {
+            case R.id.tvHello:
+                message= "edit Hello";
+                break;
+            case R.id.tvCenas:
+                message = "edit Cenas";
+                break;
+            default:
+                message = "edit Unknown";
+        }
+        showMessage(message);
+    }
+
+    private void deleteDetail(int viewId) {
+        String message;
+        switch (viewId) {
+            case R.id.tvHello:
+                message= "delete Hello";
+                break;
+            case R.id.tvCenas:
+                message = "delete Cenas";
+                break;
+            default:
+                message = "delete Unknown";
+        }
+        showMessage(message);
+    }
+
+    private void showDetail(int viewId) {
+        String message;
+        switch (viewId) {
+            case R.id.tvHello:
+                message= "show detail of Hello";
+                break;
+            case R.id.tvCenas:
+                message = "show detail of Cenas";
+                break;
+            default:
+                message = "show Unknown";
+        }
+        showMessage(message);
+    }
+
+    private void showMessage(String message) {
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
