b8065a24e5296c0e99b77d45d47df419a3280e3a
rmmcosta <ricardocosta101085@gmail.com>
2020-10-02 08:37:22 +0100

    action mode example

--- MenuApp/app/src/main/java/com/example/menuapp/MainActivity.java
+++ MenuApp/app/src/main/java/com/example/menuapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,10 +15,59 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActionMode actionMode;
+
+    class MyActionModeCallback implements ActionMode.Callback {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater menuInflater = mode.getMenuInflater();
+            menuInflater.inflate(R.menu.menu_action_mode, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action1Id:
+                    showMessage("perform action 1");
+                    mode.finish();
+                    return true;
+                default:
+                    showMessage("perform default action");
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            actionMode = null;
+        }
+    }
+
+
     private TextView tvHello;
     private TextView tvCenas;
+    private TextView tvActionMode;
     private int selectedTextViewId;
     private static final String LOG = "MyLog";
+    private ActionMode.Callback actionModeCallback = new MyActionModeCallback();
+    private View.OnLongClickListener actionModeClick = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            if (actionMode != null) {
+                return false;
+            } else {
+                actionMode = MainActivity.this.startActionMode(actionModeCallback);
+                v.setSelected(true);
+                return true;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tvHello = findViewById(R.id.tvHello);
         tvCenas = findViewById(R.id.tvCenas);
+        tvActionMode = findViewById(R.id.tvActionMode);
+        tvActionMode.setOnLongClickListener(actionModeClick);
         registerForContextMenu(tvHello);
         registerForContextMenu(tvCenas);
     }
