2e855fd1b4b1a5098b16cb60146f81e472b5a089
rmmcosta <ricardocosta101085@gmail.com>
2020-10-07 08:57:25 +0100

    first app with nav drawer

--- NavDrawerExperiment/app/src/main/java/com/example/navdrawerexperiment/MainActivity.java
+++ NavDrawerExperiment/app/src/main/java/com/example/navdrawerexperiment/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.navdrawerexperiment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*final androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
     }
 }
