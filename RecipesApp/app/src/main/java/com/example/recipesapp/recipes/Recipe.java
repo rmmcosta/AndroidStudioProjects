f03a37456982ec4db0b80650db296200fd046c74
rmmcosta <ricardocosta101085@gmail.com>
2020-10-15 08:31:18 +0100

    recipes app - the recipe class. start doing the layouts.

--- RecipesApp/app/src/main/java/com/example/recipesapp/recipes/Recipe.java
+++ RecipesApp/app/src/main/java/com/example/recipesapp/recipes/Recipe.java
@@ -1,4 +1,48 @@
 package com.example.recipesapp.recipes;
 
+import android.media.Image;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+enum Unit {g, lt}
+
 public class Recipe {
+    private Image image;
+    private List<Ingredient> ingredientList;
+    private String title, description;
+
+    public Recipe(Image image, String title, String description) {
+        this.image = image;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Recipe(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public void addIngredient(int quantity, Unit unit, String name) {
+        ingredientList.add(new Ingredient(quantity, name, unit));
+    }
+
+    public class Ingredient {
+        private int quantity;
+        private String name;
+        private Unit unit;
+
+        public Ingredient(int quantity, String name, Unit unit) {
+            this.quantity = quantity;
+            this.name = name;
+            this.unit = unit;
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            return String.valueOf(quantity) + " " + unit + " " + name;
+        }
+    }
 }
