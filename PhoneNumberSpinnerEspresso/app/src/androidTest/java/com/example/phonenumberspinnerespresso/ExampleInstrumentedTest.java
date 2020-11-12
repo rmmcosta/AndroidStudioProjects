c531b11bb17bc5165e12995071b5fa3b5307dc89
rmmcosta <ricardocosta101085@gmail.com>
2020-11-12 10:05:54 +0000

    finished testing two activities and starting tests with spinners (adapter views)

---
+++ PhoneNumberSpinnerEspresso/app/src/androidTest/java/com/example/phonenumberspinnerespresso/ExampleInstrumentedTest.java
@@ -0,0 +1,39 @@
+package com.example.phonenumberspinnerespresso;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.phonenumberspinnerespresso", appContext.getPackageName());
+    }
+
+    @Test
+    public void existsSpinner() {
+        onView(withId(R.id.spinnerPhoneType)).check(matches(isDisplayed()));
+    }
+}
---
+++ PhoneNumberSpinnerEspresso/app/src/main/java/com/example/phonenumberspinnerespresso/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.phonenumberspinnerespresso;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etPhone;
+    private TextView tvResult;
+
+    class PhoneTypeListener implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            String phone = String.valueOf(etPhone.getText());
+            if (phone.isEmpty()) {
+                tvResult.setText("");
+                return;
+            }
+            String[] phoneTypes = getResources().getStringArray(R.array.phone_types);
+            String result = String.format("%s %s", phone, phoneTypes[i]);
+            tvResult.setText(result);
+            Toast toast = Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+            tvResult.setText("");
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etPhone = findViewById(R.id.etPhone);
+        tvResult = findViewById(R.id.tvResult);
+        Spinner spinnerPhoneType = findViewById(R.id.spinnerPhoneType);
+        spinnerPhoneType.setOnItemSelectedListener(new PhoneTypeListener());
+        ArrayAdapter<CharSequence> animalsArrayAdapter = ArrayAdapter.createFromResource(this, R.array.animals, R.layout.support_simple_spinner_dropdown_item);
+        Spinner spinnerAnimals = findViewById(R.id.spinnerAnimals);
+        spinnerAnimals.setAdapter(animalsArrayAdapter);
+    }
+}
