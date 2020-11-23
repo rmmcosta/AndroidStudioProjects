30a2d3a46cab609859531945489d85063ce74255
rmmcosta <ricardocosta101085@gmail.com>
2020-11-23 20:29:49 +0000

    updating a progress bar with async task

--- SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.java
+++ SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -1,50 +1,95 @@
 package com.example.simpleasynctask;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.net.URL;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.lang.ref.WeakReference;
 
 import static android.widget.Toast.LENGTH_LONG;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvOperation;
+    private static final String TV_OPERATION_TEXT = "TV_OPERATION_TEXT";
+    private ProgressBar progressBar;
+    private ProgressBar progressBar2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progressBar);
+        tvOperation = findViewById(R.id.tvOperation);
+        if (savedInstanceState != null && !savedInstanceState.getString(TV_OPERATION_TEXT).equals("")) {
+            tvOperation.setText(savedInstanceState.getString(TV_OPERATION_TEXT));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(TV_OPERATION_TEXT, tvOperation.getText().toString());
     }
 
     public void startTask(View view) {
-        new MyAsyncTask(900, this).execute();
+        int sleepInMs = 200;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            progressBar.setMin(0);
+        }
+        progressBar.setMax(sleepInMs);
+        new MyAsyncTask(sleepInMs, this, tvOperation, progressBar).execute();
     }
 
-    private static class MyAsyncTask extends AsyncTask<URL, Integer, String> {
-        private int sleepInMs;
-        private Context context;
+    private static class MyAsyncTask extends AsyncTask<Void, Integer, String> {
+        private final int sleepInMs;
+        private final Context context;
+        private final WeakReference<TextView> tvResult;
+        private final WeakReference<ProgressBar> pb1;
 
-        private MyAsyncTask(int sleepInMs, Context context) {
+        private MyAsyncTask(int sleepInMs, Context context, TextView tvResult, ProgressBar pb1) {
             this.sleepInMs = sleepInMs;
             this.context = context;
+            this.tvResult = new WeakReference<>(tvResult);
+            this.pb1 = new WeakReference<>(pb1);
         }
 
         @Override
-        protected String doInBackground(URL... urls) {
+        protected void onPreExecute() {
+            tvResult.get().setText(R.string.while_running_async_task);
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
             try {
-                Thread.sleep(sleepInMs);
+                int count = 0;
+                while (count++ < sleepInMs) {
+                    Thread.sleep(1);
+                    publishProgress(count);
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            return "Finished";
+            return String.format(context.getString(R.string.async_task_result), sleepInMs);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            tvResult.get().setText(String.format(context.getString(R.string.while_running_async_task), values[0]));
+            pb1.get().setProgress(values[0]);
         }
 
         @Override
         protected void onPostExecute(String s) {
+            tvResult.get().setText(s);
             Toast toast = Toast.makeText(context, s, LENGTH_LONG);
             toast.show();
         }
