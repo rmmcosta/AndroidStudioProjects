c0dc555236090638359b84bf0630561c0cd18b39
rmmcosta <ricardocosta101085@gmail.com>
2020-12-01 19:08:26 +0000

    start working on the bits and pizzas (chapter 9 - head first Android. Themes and action bar.

---
+++ BitsandPizzas/app/src/main/java/com/example/bitsandpizzas/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.bitsandpizzas;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
