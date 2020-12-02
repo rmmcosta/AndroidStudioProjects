ec795669bfbff50967931a845bb09d91baecb897
rmmcosta <ricardocosta101085@gmail.com>
2020-12-02 08:48:14 +0000

    start new activity on create order in bits and pizzas. Different theme for that specific activity

--- BitsandPizzas/app/src/main/java/com/example/bitsandpizzas/MainActivity.java
+++ BitsandPizzas/app/src/main/java/com/example/bitsandpizzas/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.bitsandpizzas;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -24,6 +26,27 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.createOrderMenuOption:
+                showToast("Create Order");
+                launchOrderActivity();
+                return true;
+            case R.id.settingsMenuOption:
+                showToast("Settings");
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void launchOrderActivity() {
+        Intent intent = new Intent(this, OrderActivity.class);
+        startActivity(intent);
+    }
+
+    private void showToast(String text) {
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
