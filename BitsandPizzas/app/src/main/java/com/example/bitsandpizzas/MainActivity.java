bb7a5f7a826f0cc517e2139d6b77945382599017
rmmcosta <ricardocosta101085@gmail.com>
2020-12-04 08:59:43 +0000

    more on bits and pizzas with navigation drawer and fragments. Some issues were found trying to use a list view inside the drawer layout and now the navigation view was included instead

--- BitsandPizzas/app/src/main/java/com/example/bitsandpizzas/MainActivity.java
+++ BitsandPizzas/app/src/main/java/com/example/bitsandpizzas/MainActivity.java
@@ -1,32 +1,87 @@
 package com.example.bitsandpizzas;
 
-import android.app.Activity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.view.MenuItemCompat;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.ShareActionProvider;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
+import androidx.core.view.MenuItemCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
 
     private ShareActionProvider shareActionProvider;
+
+    DrawerLayout drawerLayout;
+    FrameLayout frameLayout;
+    private NavigationView nvDrawer;
+    private String[] drawerCategoriesArray;
+
+    class DrawerOnClick implements NavigationView.OnNavigationItemSelectedListener {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            int itemId = item.getItemId();
+            if (itemId == R.id.niPasta) {
+                fragment = new PastaFragment();
+            } else if (itemId == R.id.niPizzas) {
+                fragment = new PizzasFragment();
+            } else if (itemId == R.id.niStores) {
+                fragment = new StoresFragment();
+            } else {
+                fragment = new TopFragment();
+            }
+            loadFragment(fragment);
+            return true;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeDrawer();
+        initializeDefaultFragment();
+    }
+
+    private void initializeDefaultFragment() {
+        loadFragment(new TopFragment());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameLayout, fragment);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    private void initializeDrawer() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        frameLayout = findViewById(R.id.frameLayout);
+        nvDrawer = findViewById(R.id.navigationView);
+        nvDrawer.setNavigationItemSelectedListener(new DrawerOnClick());
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu, menu);
-        MenuItem shareMenuItem = menu.findItem(R.id.shareActionMenuOption);
-        shareActionProvider = (ShareActionProvider) shareMenuItem.getActionProvider();
+        MenuItem shareItem = menu.findItem(R.id.shareActionMenuOption);
+        shareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
         defineIntent();
         return super.onCreateOptionsMenu(menu);
     }
@@ -34,17 +89,15 @@ public class MainActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
-        switch (itemId) {
-            case R.id.createOrderMenuOption:
-                showToast("Create Order");
-                launchOrderActivity();
-                return true;
-            case R.id.settingsMenuOption:
-                showToast("Settings");
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if (itemId == R.id.createOrderMenuOption) {
+            showToast("Create Order");
+            launchOrderActivity();
+            return true;
+        } else if (itemId == R.id.settingsMenuOption) {
+            showToast("Settings");
+            return true;
         }
+        return super.onOptionsItemSelected(item);
     }
 
     private void defineIntent() {
---
+++ BitsandPizzas/app/src/main/java/com/example/bitsandpizzas/TopFragment.java
@@ -0,0 +1,23 @@
+package com.example.bitsandpizzas;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TopFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_top, container, false);
+    }
+}
