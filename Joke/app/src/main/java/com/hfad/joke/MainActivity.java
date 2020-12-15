7e374a31903bf427227e3d42f3f2e8c33628b35a
rmmcosta <ricardocosta101085@gmail.com>
2020-12-15 09:11:57 +0000

    Joke and NotifyMe - notifications, with (api 26 or above) and without channels

--- Joke/app/src/main/java/com/hfad/joke/MainActivity.java
+++ Joke/app/src/main/java/com/hfad/joke/MainActivity.java
@@ -1,13 +1,23 @@
 package com.hfad.joke;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String NOTIFICATION_CHANNEL_ID = "MY_NOTIFICATION_CHANNEL_123";
+    private static final String NOTIFICATION_CHANNEL_NAME = "MY_NOTIFICATION_CHANNEL";
+    private static final int NOTIFICATION_ID = 1234;
     private Intent bindIntent;
 
     @Override
@@ -35,5 +45,34 @@ public class MainActivity extends AppCompatActivity {
     public void startBindService(View view) {
         bindIntent = new Intent(this, MyBindService.class);
         startService(bindIntent);
+    }
+
+    public void sendNotification26Plus(View view) {
+        if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            notify26Plus();
+        } else {
+            sendNotification(view);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void notify26Plus() {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        NotificationChannel notificationChannel = null;
+        notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        Notification notification = builder
+                .setSmallIcon(R.drawable.ic_stat_notify)
+                .setContentTitle("The Title")
+                .setContentText("This is the content text")
+                .setChannelId(NOTIFICATION_CHANNEL_ID)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .build();
+        notificationManager.createNotificationChannel(notificationChannel);
+        notificationManager.notify(NOTIFICATION_ID, notification);
     }
 }
--- NotifyMe/app/src/main/java/com/hfad/notifyme/MainActivity.java
+++ NotifyMe/app/src/main/java/com/hfad/notifyme/MainActivity.java
@@ -3,6 +3,8 @@ package com.hfad.notifyme;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -17,21 +19,28 @@ public class MainActivity extends AppCompatActivity {
     private static final int NOTIFICATION_ID = 0;
     private NotificationManager notificationManager;
     private NotificationCompat.Builder notificationBuilder;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intent = new Intent(this, MainActivity.class);
         initializeNotificationManager();
         initializeNotificationBuilder();
     }
 
     private void initializeNotificationBuilder() {
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         notificationBuilder = new NotificationCompat
                 .Builder(this, NOTIFICATION_CHANNEL_ID)
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.drawable.ic_stat_notify)
                 .setContentTitle("My Notification")
-                .setContentText("This is my notification to you");
+                .setContentText("This is my notification to you")
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
     }
 
     public void notifyMe(View view) {
