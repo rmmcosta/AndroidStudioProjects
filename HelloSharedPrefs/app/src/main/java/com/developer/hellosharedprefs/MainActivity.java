a6b0ba2587d0972ca03f3436fcdfb3f87bd1bbe4
rmmcosta <ricardocosta101085@gmail.com>
2020-12-27 20:06:53 +0000

    using shared preferences to store app data. Even after closing the app or rebooting the device

--- HelloSharedPrefs/app/src/main/java/com/developer/hellosharedprefs/MainActivity.java
+++ HelloSharedPrefs/app/src/main/java/com/developer/hellosharedprefs/MainActivity.java
@@ -1,7 +1,9 @@
 package com.developer.hellosharedprefs;
 
+import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,17 +21,19 @@ public class MainActivity extends AppCompatActivity {
     private int count = COUNT_INITIAL;
     private int currentColorButtonId;
     private TextView tvCount;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         tvCount = findViewById(R.id.tvCount);
-        if (savedInstanceState != null) {
-            changeColor(savedInstanceState.getInt(SAVE_COLOR_BUTTON_ID));
-            count = savedInstanceState.getInt(SAVE_COUNT);
-            updateCountTextView();
-        }
+        checkSavedState(savedInstanceState);
+
+        preferences = getPreferences(MODE_PRIVATE); //mode_private - the file can only be accessed by the calling application
+        checkSavedPreferences();
+
     }
 
     @Override
@@ -37,6 +41,38 @@ public class MainActivity extends AppCompatActivity {
         outState.putInt(SAVE_COLOR_BUTTON_ID, currentColorButtonId);
         outState.putInt(SAVE_COUNT, count);
         super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onPause() {
+        savePreferences();
+        super.onPause();
+    }
+
+    private void savePreferences() {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(SAVE_COLOR_BUTTON_ID, currentColorButtonId);
+        editor.putInt(SAVE_COUNT, count);
+        boolean success = editor.commit();
+        Log.d(LOG_TAG, String.format("preferences committed with success: %s", success ? "True" : "False"));
+    }
+
+    private void checkSavedPreferences() {
+        if (preferences.contains(SAVE_COLOR_BUTTON_ID)) {
+            changeColor(preferences.getInt(SAVE_COLOR_BUTTON_ID, COLOR_DEFAULT));
+        }
+        if (preferences.contains(SAVE_COUNT)) {
+            count = preferences.getInt(SAVE_COUNT, COUNT_INITIAL);
+            updateCountTextView();
+        }
+    }
+
+    private void checkSavedState(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            changeColor(savedInstanceState.getInt(SAVE_COLOR_BUTTON_ID));
+            count = savedInstanceState.getInt(SAVE_COUNT);
+            updateCountTextView();
+        }
     }
 
     public void changeColor(View view) {
