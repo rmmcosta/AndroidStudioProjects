4f61a2f3bc8b20c863fa371cc8a8914227e17b68
rmmcosta <ricardocosta101085@gmail.com>
2020-12-29 10:31:14 +0000

    Droid cafe with settings - reading the default shared preferences

--- DroidCafe_withSettings/app/src/main/java/com/example/droidcafe_withsettings/ConfirmPurchase.java
+++ DroidCafe_withSettings/app/src/main/java/com/example/droidcafe_withsettings/ConfirmPurchase.java
@@ -1,10 +1,13 @@
 package com.example.droidcafe_withsettings;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,19 +20,24 @@ import java.util.Set;
 public class ConfirmPurchase extends AppCompatActivity {
     private RecyclerView rvCart;
 
-    private void showCartItems(Context context) {
-        Set<Map.Entry<String, Integer>> cartItems = MainActivity.getCartItems();
-        CartItemsAdapter cartItemsAdapter = new CartItemsAdapter(cartItems);
-        rvCart.setAdapter(cartItemsAdapter);
-        rvCart.setLayoutManager(new LinearLayoutManager(context));
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confim_purchase);
         rvCart = findViewById(R.id.rvCart);
         showCartItems(this);
+        //SharedPreferences sharedPreferences = getSharedPreferences("com.example.droidcafe_withsettings_preferences", MODE_PRIVATE);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String market = sharedPreferences.getString("market", "US");
+        TextView tvMarket = findViewById(R.id.tvMarket);
+        tvMarket.setText(market);
+    }
+
+    private void showCartItems(Context context) {
+        Set<Map.Entry<String, Integer>> cartItems = MainActivity.getCartItems();
+        CartItemsAdapter cartItemsAdapter = new CartItemsAdapter(cartItems);
+        rvCart.setAdapter(cartItemsAdapter);
+        rvCart.setLayoutManager(new LinearLayoutManager(context));
     }
 
     public void buy(View view) {
