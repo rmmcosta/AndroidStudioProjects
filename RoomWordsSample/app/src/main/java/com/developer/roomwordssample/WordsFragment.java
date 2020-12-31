2e04470d6e505279d01ee19f54a7f212bbea4430
rmmcosta <ricardocosta101085@gmail.com>
2020-12-31 10:33:14 +0000

    arfchitecture components - room, live data and view model. Room Words Sample - first steps.

---
+++ RoomWordsSample/app/src/main/java/com/developer/roomwordssample/WordsFragment.java
@@ -0,0 +1,32 @@
+package com.developer.roomwordssample;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class WordsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_words, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(viewFab -> NavHostFragment.findNavController(WordsFragment.this)
+                .navigate(R.id.action_FirstFragment_to_SecondFragment));
+    }
+}
